ec55b65d3e3ef9ab8449754ecd3f147432ff457c
Mbithi Raymond Kombo <121002402+Komboray@users.noreply.github.com>
2023-10-24 17:47:55 +0300

    Update links.js

--- links.js
+++ links.js
@@ -1,5 +1,16 @@
 var tablinks =document.getElementsByClassName("tab-links");
 var tabcontents =document.getElementsByClassName("tab-contents")
+
+
+var navLinks = document.getElementById("navLinks");
+function showMenu(){
+    navLinks.style.right = "0";
+}
+
+function hideMenu(){
+    navLinks.style.right = "-200px";
+ }
+
 function opentab(tabname){
     for(tablink of tablinks){
         tablink.classList.remove("active-link");
